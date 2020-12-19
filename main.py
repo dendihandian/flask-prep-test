a90bd43d6f43039db395043004ad227f92f5f32e
Dendi Handian <dendihandian@gmail.com>
2020-12-19 21:30:18 +0700

    to main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from config.app import APP_DEBUG, APP_SECRET_KEY
 from flask import Flask
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = APP_SECRET_KEY
 
 @app.route('/')
 def hello():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=APP_DEBUG)
